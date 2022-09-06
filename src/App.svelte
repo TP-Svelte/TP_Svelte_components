5f5df3aab2be373ddb74bcb8a12e0daac5331299
NicolasFOESSEL <nicofoessel91740@gmail.com>
2022-09-06 11:28:31 +0200

    tableau "selectCode"
    Création et auto complétion du tableau selectCode[]

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
 // variables
     let number = 1;
     let numberValidate = false;
+    let codeValidate = false;
+    let code = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    //let selectCode;
 
 // fonctions
     // choix nombre
@@ -18,12 +21,28 @@
     };
 
     // validation nombre
+    const sizeTable = () => {
+        let selectCode = new Array(number);
+        console.log(selectCode);
+        for(let i = 0; i < selectCode.length; i++){
+            selectCode[i] = code[i];
+        }
+        console.log(selectCode);
+
+    }
+
     const validateChoiceNumber = () => {
         numberValidate = true;
+        sizeTable();
     };
 
     const disableValidateChoiceNumber = () => {
         numberValidate = false;
+    }
+
+    // validation confirmation
+    const validateCode = () => {
+        codeValidate = true;
     }
 
 </script>
@@ -49,11 +68,42 @@
 {/if}
 
 
-{#if numberValidate === true}
+{#if numberValidate === true && codeValidate === false }
+
     <p>Chiffre / nombre retenu : { number }</p>
+
+    {#each Array(number) as _, i}
+        <div>
+            <button on:click={ supNumber }>
+                +
+            </button>
+            
+            <p>test</p> 
+            
+            <button on:click={ subNumber }>
+                -
+            </button> 
+        </div>        
+    {/each}
+
+    <button on:click={ validateCode }>
+        Valider
+    </button>
+
     <button on:click={ disableValidateChoiceNumber }>
         Retour en arrière
     </button>
+{/if}
+
+
+
+
+
+
+
+
+{#if codeValidate === true}
+    <p>Félicitations, le code a bien été validé !</p>
 {/if}
 
 <style>
