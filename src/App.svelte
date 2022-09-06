36151a953d562d28cb7376637a1f2a2b025ab58e
Doustalet Chloé <cdoustalet@gmail.com>
2022-09-06 11:02:04 +0200

    Component working without CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script>
   import Feeds from "./UI/Feeds.svelte";
+  
+  let checkpoints_list = [
+      [ "Dossier envoyé", "sent", "2 sept" ],
+      [ "Dossier en cours de traitement", "loading", "2 sept" ],
+      [ "Dossier refusé", "closed", "5 sept" ],
+      [ "Dossier en cours de réévaluation", "loading", "6 sept"],
+      [ "Dossier accepté", "validated", "15 sept" ]    
+    ];
 </script>
 
 <h1>Nos composants</h1>
 <h2>Feeds</h2>
-<Feeds/>
+<Feeds checkpoints={checkpoints_list} />
--- src/UI/Feeds.svelte
+++ src/UI/Feeds.svelte
@@ -1,41 +1,28 @@
-<script>
-    let items = [
-      {
-        "content":"Dossier envoyé",
-        "status":"sent",
-        "date":1981,
-      },
-      {
-        "content":"Dossier en cours de traitement",
-        "status":"loading",
-        "date":1981,
-      },
-      {
-        "content":"Dossier refusé",
-        "status":"closed",
-        "date":1981,
-      },
-      {
-        "content":"Dossier en cours de réévaluation",
-        "status":"loading",
-        "date":1981,
-      },
-      {
-        "content":"Dossier accepté",
-        "status":"validated",
-        "date":1981,
-      }    
-    ];
-    
+<script>  
+    export let checkpoints;
+
     const sent_img = "src/assets/sent.svg";
     const loading_img = "src/assets/loading.svg";
     const closed_img = "src/assets/closed.svg";
     const validated_img = "src/assets/validated.svg";
-
 </script>
 
 <div>
-  <p>Je teste si cela fonctionne.</p>
+    {#each checkpoints as checkpoint}
+        <div>
+            {#if checkpoint[1] === "sent"}
+              <img src="{sent_img}" alt="sent icon">
+            {:else if checkpoint[1] === "loading" }
+              <img src="{loading_img}" alt="loading icon">
+            {:else if checkpoint[1] === "closed"}
+              <img src="{closed_img}" alt="closed icon">
+            {:else}
+              <img src="{validated_img}" alt="validated icon">
+            {/if}
+            <p>{checkpoint[0]}</p>
+            <p>{checkpoint[2]}</p>
+        </div>
+    {/each}
 </div>
 
 <style>
