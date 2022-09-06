35daf65d89fe7494955f089e93405f183ea41418
NicolasFOESSEL <nicofoessel91740@gmail.com>
2022-09-06 09:52:38 +0200

    Update App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+// variables
+    let number = 0;
+
+// fonctions
+    const supNumber = () => {
+        if(number < 10){
+            number ++;
+        }
+    }
+    const subNumber = () => {
+        if(number > 0){
+            number --;
+        }
+    }
+</script>
+
+
+<button on:click={ supNumber }>
+    +
+</button>
+
+
+<p>{number}</p>
+
+<button on:click={ subNumber }>
+    -
+</button>
+
+
+<style>
+
+</style>
