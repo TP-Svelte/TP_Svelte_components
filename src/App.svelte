e48a423346e0966020d5f6b1728fef973dd63104
NicolasFOESSEL <nicofoessel91740@gmail.com>
2022-09-06 12:08:22 +0200

    Fonctionnalités terminées

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
     let numberValidate = false;
     let codeValidate = false;
     let code = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-    //let selectCode;
+    let selectCode;
 
 // fonctions
     // choix nombre
@@ -22,18 +22,17 @@
 
     // validation nombre
     const sizeTable = () => {
-        let selectCode = new Array(number);
+        selectCode = new Array(number);
         console.log(selectCode);
         for(let i = 0; i < selectCode.length; i++){
             selectCode[i] = code[i];
         }
-        console.log(selectCode);
-
+        code.splice(selectCode.length);
     }
 
     const validateChoiceNumber = () => {
-        numberValidate = true;
         sizeTable();
+        numberValidate = true;
     };
 
     const disableValidateChoiceNumber = () => {
@@ -41,8 +40,23 @@
     }
 
     // validation confirmation
+    const supNumberCode = (i) => {
+        if( selectCode[i] < 10 ){
+            selectCode[i] ++;
+        } 
+    }
+    const subNumberCode = (i) => {
+        if ( selectCode[i] > 1 ) {
+            selectCode[i] -= 1;
+        }
+
+    }
+
     const validateCode = () => {
-        codeValidate = true;
+        if ( JSON.stringify(selectCode) === JSON.stringify(code) ) {
+            codeValidate = true;
+            console.log("ok");
+        }
     }
 
 </script>
@@ -74,13 +88,21 @@
 
     {#each Array(number) as _, i}
         <div>
-            <button on:click={ supNumber }>
+            <button on:click={ 
+             (() => {
+                supNumberCode(i);
+                })
+            }>
                 +
             </button>
             
-            <p>test</p> 
+            <p>{ selectCode[i] }</p> 
             
-            <button on:click={ subNumber }>
+            <button on:click={ 
+                (() => {
+                    subNumberCode(i);
+                })
+            }>
                 -
             </button> 
         </div>        
