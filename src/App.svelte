afd31aacf2226245d295472decdffbab02c73801
NicolasFOESSEL <nicofoessel91740@gmail.com>
2022-09-06 10:16:46 +0200

    Validation
    Ajout de la validation du chiffre / nombre

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,60 @@
 <script>
 // variables
-    let number = 0;
+    let number = 1;
+    let numberValidate = false;
 
 // fonctions
+    // choix nombre
     const supNumber = () => {
         if(number < 10){
             number ++;
         }
-    }
+    };
+
     const subNumber = () => {
-        if(number > 0){
+        if(number > 1){
             number --;
         }
+    };
+
+    // validation nombre
+    const validateChoiceNumber = () => {
+        numberValidate = true;
+    };
+
+    const disableValidateChoiceNumber = () => {
+        numberValidate = false;
     }
+
 </script>
 
+{#if numberValidate === false}
+    <p>Choisissez le nombre de case de code de vérification</p>
 
-<button on:click={ supNumber }>
-    +
-</button>
+    <div>
+        <button on:click={ supNumber }>
+            +
+        </button>
+        
+        <p>{number}</p>
+        
+        <button on:click={ subNumber }>
+            -
+        </button>
+        
+    </div>
+    <button on:click={ validateChoiceNumber }>
+        Valider
+    </button>
+{/if}
 
 
-<p>{number}</p>
-
-<button on:click={ subNumber }>
-    -
-</button>
-
+{#if numberValidate === true}
+    <p>Chiffre / nombre retenu : { number }</p>
+    <button on:click={ disableValidateChoiceNumber }>
+        Retour en arrière
+    </button>
+{/if}
 
 <style>
 
