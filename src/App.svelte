24f972a03b9130397bc5b2ef24e357a3d83b5e32
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-09-06 09:51:09 +0200

    [ADD]UserCard

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+    import UserCard from "./components/UI/UserCard.svelte";
+
+    let persons = [{
+        name: 'Maxime',
+        cover: 'https://images.unsplash.com/photo-1657664065994-5e257c88b7f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1683&q=800'
+    },
+        {name: 'Jul',
+        cover: 'https://images.unsplash.com/photo-1657664065994-5e257c88b7f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1683&q=80'
+    },
+        {name: 'Jul',
+        cover: 'https://images.unsplash.com/photo-1657664065994-5e257c88b7f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1683&q=80'
+    },
+    ]
+</script>
+
+<UserCard persons={persons} />
