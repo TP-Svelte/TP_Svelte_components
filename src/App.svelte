24d41590e858ef90cd69c9f0fe8513b2cc4a0e08
Doustalet Chloé <cdoustalet@gmail.com>
2022-09-06 10:20:58 +0200

    Creation branch

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Feeds from "./UI/Feeds.svelte";
+</script>
+
+<h1>Nos composants</h1>
+<h2>Feeds</h2>
+<Feeds/>
---
+++ src/UI/Feeds.svelte
@@ -0,0 +1,43 @@
+<script>
+    let items = [
+      {
+        "content":"Dossier envoyé",
+        "status":"sent",
+        "date":1981,
+      },
+      {
+        "content":"Dossier en cours de traitement",
+        "status":"loading",
+        "date":1981,
+      },
+      {
+        "content":"Dossier refusé",
+        "status":"closed",
+        "date":1981,
+      },
+      {
+        "content":"Dossier en cours de réévaluation",
+        "status":"loading",
+        "date":1981,
+      },
+      {
+        "content":"Dossier accepté",
+        "status":"validated",
+        "date":1981,
+      }    
+    ];
+    
+    const sent_img = "src/assets/sent.svg";
+    const loading_img = "src/assets/loading.svg";
+    const closed_img = "src/assets/closed.svg";
+    const validated_img = "src/assets/validated.svg";
+
+</script>
+
+<div>
+  <p>Je teste si cela fonctionne.</p>
+</div>
+
+<style>
+
+</style>
