913a046c98698bec0414d8f7470f053b27bc4c65
ValentinR01 <vale.vr@icloud.com>
2022-09-07 12:40:15 +0200

    Text rework

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,15 +3,8 @@
 </script>
 
 <div class="container">
-    <h1>
-    Docs
-    Components
-    Blog
-    JobsNew
-    Theme
-    Tailwind CSS on GitHub
-    Rapidly build modern websites without ever leaving your HTML.</h1>
-    <p>A utility-first CSS framework packed with classes like flex, pt-4, text-center and <br> rotate-90 that can be composed to build any design, directly in your markup.</p>
+    <h1>Svelteness</h1>
+    <p>Discover this collection of ready-to-use components.<br> Made by Hetic students.</p>
 </div>
 <style>
 .container {
