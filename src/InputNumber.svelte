10ba5e9a7d3b13f08c64cbce113709549b5dc0c2
NicolasFOESSEL <nicofoessel91740@gmail.com>
2022-09-07 09:46:40 +0200

    Début composant Steps
    Début de la création du composant "Steps"

---
+++ src/InputNumber.svelte
@@ -0,0 +1,134 @@
+<script>
+    // variables
+        let number = 1;
+        let numberValidate = false;
+        let codeValidate = false;
+        let code = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        let selectCode;
+    
+    // fonctions
+        // choix nombre
+        const supNumber = () => {
+            if(number < 10){
+                number ++;
+            }
+        };
+    
+        const subNumber = () => {
+            if(number > 1){
+                number --;
+            }
+        };
+    
+        // validation nombre
+        const sizeTable = () => {
+            selectCode = new Array(number);
+            console.log(selectCode);
+            for(let i = 0; i < selectCode.length; i++){
+                selectCode[i] = code[i];
+            }
+            code.splice(selectCode.length);
+        }
+    
+        const validateChoiceNumber = () => {
+            sizeTable();
+            numberValidate = true;
+        };
+    
+        const disableValidateChoiceNumber = () => {
+            numberValidate = false;
+        }
+    
+        // validation confirmation
+        const supNumberCode = (i) => {
+            if( selectCode[i] < 9 ){
+                selectCode[i] ++;
+            } 
+        }
+    
+        const subNumberCode = (i) => {
+            if ( selectCode[i] > 0 ) {
+                selectCode[i] -= 1;
+            }
+    
+        }
+    
+        const validateCode = () => {
+            if ( JSON.stringify(selectCode) === JSON.stringify(code) ) {
+                codeValidate = true;
+            }
+        }
+    
+    </script>
+    
+    
+    {#if numberValidate === false}
+        <p>Choisissez le nombre de case de code de vérification</p>
+    
+        <div>
+            <button on:click={ supNumber }>
+                +
+            </button>
+            
+            <p>{number}</p>
+            
+            <button on:click={ subNumber }>
+                -
+            </button>
+            
+        </div>
+        <button on:click={ validateChoiceNumber }>
+            Valider
+        </button>
+    {/if}
+    
+    
+    {#if numberValidate === true && codeValidate === false }
+    
+        <p>Chiffre / nombre retenu : { number }</p>
+    
+        {#each Array(number) as _, i}
+            <div>
+                <button on:click={ 
+                 (() => {
+                    supNumberCode(i);
+                    })
+                }>
+                    +
+                </button>
+                
+                <p>{ selectCode[i] }</p> 
+                
+                <button on:click={ 
+                    (() => {
+                        subNumberCode(i);
+                    })
+                }>
+                    -
+                </button> 
+            </div>        
+        {/each}
+    
+        <button on:click={ validateCode }>
+            Valider
+        </button>
+    
+        <button on:click={ disableValidateChoiceNumber }>
+            Retour en arrière
+        </button>
+    {/if}
+    
+    
+    
+    
+    
+    
+    
+    
+    {#if codeValidate === true}
+        <p>Félicitations, le code a bien été validé !</p>
+    {/if}
+    
+    <style>
+    
+    </style>
---
+++ src/Steps.svelte
@@ -0,0 +1,57 @@
+<script>
+// variables
+    let homePage = true;
+    let codePage = false;
+    let finishPage = false;
+
+// fonctions
+
+</script>
+
+<section>
+    <div class="homePage">
+        <!-- {#if homePage === true} -->
+        <div class="line"></div>
+        <p>
+            Step 1
+        </p>
+        <p>
+            Choix de la taille du code
+        </p>
+        <!-- {/if} -->
+
+    </div>
+    <div class="codePage">
+        <!-- {#if codePage === true} -->
+            <div class="line"></div>
+            <p>
+                Step 2
+            </p>
+            <p>
+                Entrée du code de sécurité
+            </p>
+        <!-- {/if} -->
+
+    </div>
+    <div class="finishPage">
+        <!-- {#if finishPage === true} -->
+        <div class="line"></div>
+        <p>
+            Step 3
+        </p>
+        <p>
+            Page de fin
+        </p>
+    
+        <!-- {/if} -->
+
+    </div>
+
+
+</section>
+
+
+
+<style>
+    
+</style>
